d64248789dbd7dede7fced85625d1cf29a50dfef
Daniele Olmisani <daniele.olmisani@gmail.com>
2014-01-19 18:34:27 +0100

    created in repository

---
+++ SillyIdentyIcons/gen/dolmisani/android/utils/sillyidentyicons/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package dolmisani.android.utils.sillyidentyicons;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
